b1b731aa51820cdf2439ddff1e89d34467b8ae4d
Tony Dantona <tonydantona@gmail.com>
2025-12-15 17:06:13 -0500

    Add comment to __init__.py for testing worktree flow (#2)

---
+++ src/api/__init__.py
@@ -0,0 +1,4 @@
+# sometimes used in a package (directory) to store configuration settings 
+# or other data that is used by the package
+# the __init__.py file can also be used to initialize the package 
+# and set up any necessary variables or functions
